f22e624c068f17e660a244dda921bacfbe74afb0
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-09-09 21:58:58 +0300

    decluttered kanban main page

--- src/apps/kanban/page.tsx
+++ src/apps/kanban/page.tsx
@@ -1,6 +1,6 @@
-import TasksCard from "./components/TasksCard";
 import AddNewDialog from "./components/AddNewDialog";
 import useTasksStore from "./store/useTasksStore";
+import Column from "./components/Column";
 
 export default function Kanban() {
   const { todoTasks, inProgress, done } = useTasksStore();
@@ -16,31 +16,9 @@ export default function Kanban() {
       </div>
       {/* three columns */}
       <div className="grid md:grid-cols-3 gap-2 my-4 min-h-[50vh]">
-        <div className="bg-sky-200 p-2 shadow-sm rounded-sm">
-          <h2 className="mb-2 font-bold text-md">
-            To-Do {todoTasks.length > 0 && <span>({todoTasks.length})</span>}
-          </h2>
-          {todoTasks.map((task) => (
-            <TasksCard key={task.id} taskName={task.taskName} taskId={task.id} />
-          ))}
-        </div>
-        <div className="bg-amber-200 p-2 shadow-sm rounded-sm">
-          <h2 className="mb-2 font-bold text-md">
-            In Progress{" "}
-            {inProgress.length > 0 && <span>({inProgress.length})</span>}
-          </h2>
-          {inProgress.map((task) => (
-            <TasksCard key={task.id} taskName={task.taskName} taskId={task.id} />
-          ))}
-        </div>
-        <div className="bg-teal-200 p-2 shadow-sm rounded-sm">
-          <h2 className="mb-2 font-bold text-md">
-            Done {done.length > 0 && <span>({done.length})</span>}
-          </h2>
-          {done.map((task) => (
-            <TasksCard key={task.id} taskName={task.taskName} taskId={task.id} />
-          ))}
-        </div>
+        <Column title="To-Do" color="bg-sky-200" tasks={todoTasks} />
+        <Column title="In Progress" color="bg-amber-200" tasks={inProgress} />
+        <Column title="Done" color="bg-teal-200" tasks={done} />
       </div>
     </div>
   );
