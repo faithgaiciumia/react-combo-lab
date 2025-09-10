60bd8d7d4cac58be3ffef0f9cef08ad520bab792
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-09-10 07:44:00 +0300

    edit and delete dropdowns

--- src/apps/kanban/components/TasksCard.tsx
+++ src/apps/kanban/components/TasksCard.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
+import { Edit, MoreVertical, Trash } from "lucide-react";
+import useTasksStore from "../store/useTasksStore";
 export default function TasksCard({
   taskName,
   taskId,
@@ -7,14 +16,16 @@ export default function TasksCard({
   taskName: string;
   taskId: number;
 }) {
-  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
-    id: taskId.toString(),
-  });
+  const { attributes, listeners, setNodeRef, transform, isDragging } =
+    useDraggable({
+      id: taskId.toString(),
+    });
   const style = {
     transform: CSS.Translate.toString(transform),
-    opacity:isDragging?0.5:1,
-    cursor:"grab"
+    opacity: isDragging ? 0.9 : 1,
+    cursor: "grab",
   };
+  const { removeTask } = useTasksStore();
   return (
     <div
       className="bg-white p-2 rounded-md"
@@ -23,7 +34,34 @@ export default function TasksCard({
       {...listeners}
       {...attributes}
     >
-      <span className="text-sm">{taskName}</span>
+      <div className="flex items-center justify-between">
+        <span className="text-sm">{taskName}</span>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MoreVertical />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="space-y-2">
+            <DropdownMenuItem asChild className="justify-start">
+              <Button
+                variant={"ghost"}
+                className="text-blue-600 w-[100%] hover:text-blue-800"
+              >
+                <Edit color="blue" /> Edit
+              </Button>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild className="justify-start">
+              <Button
+                variant={"ghost"}
+                onClick={() => removeTask(taskId)}
+                className="text-red-600 w-[100%] hover:text-red-800"
+              >
+                <Trash color="red" />
+                Delete
+              </Button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
--- src/apps/kanban/page.tsx
+++ src/apps/kanban/page.tsx
@@ -1,9 +1,23 @@
 import AddNewDialog from "./components/AddNewDialog";
 import useTasksStore from "./store/useTasksStore";
 import Column from "./components/Column";
+import { DndContext, type DragEndEvent } from "@dnd-kit/core";
 
 export default function Kanban() {
-  const { todoTasks, inProgress, done } = useTasksStore();
+  const { todoTasks, inProgress, done, moveTask } = useTasksStore();
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { over, active } = e;
+
+    //handle outside droppable edge case
+    if (!over) return;
+
+    //get task and column
+    const taskId = Number(active.id);
+    const newStatus = over.id as "todo" | "inProgress" | "done";
+
+    //update state
+    moveTask(taskId, newStatus);
+  };
 
   return (
     <div className="p-4">
@@ -16,11 +30,23 @@ export default function Kanban() {
         <AddNewDialog />
       </div>
       {/* three columns */}
-      <div className="grid md:grid-cols-3 gap-2 my-4 min-h-[50vh]">
-        <Column title="To-Do" color="bg-sky-200" tasks={todoTasks} id="todo" />
-        <Column title="In Progress" color="bg-amber-200" tasks={inProgress} id="inProgress" />
-        <Column title="Done" color="bg-teal-200" tasks={done} id="done" />
-      </div>
+      <DndContext onDragEnd={handleDragEnd}>
+        <div className="grid md:grid-cols-3 gap-2 my-4 min-h-[50vh]">
+          <Column
+            title="To-Do"
+            color="bg-sky-200"
+            tasks={todoTasks}
+            id="todo"
+          />
+          <Column
+            title="In Progress"
+            color="bg-amber-200"
+            tasks={inProgress}
+            id="inProgress"
+          />
+          <Column title="Done" color="bg-teal-200" tasks={done} id="done" />
+        </div>
+      </DndContext>
     </div>
   );
 }
