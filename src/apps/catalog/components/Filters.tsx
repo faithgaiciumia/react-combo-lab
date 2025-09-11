e36652b509ecc836bfe12db00ea28d5556be7b26
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-09-11 08:26:42 +0300

    implemented DRY on filter component

--- src/apps/catalog/components/Filters.tsx
+++ src/apps/catalog/components/Filters.tsx
@@ -6,59 +6,28 @@ export default function Filters({
 }: {
   applyFilter: (category: string) => void;
 }) {
+  const categories = ["All", "Electronics", "Accessories", "Books"];
   const [activeCategory, setActiveCategory] = useState("All");
 
   return (
     <div className="flex items-center justify-center p-2 bg-purple-300 my-2">
       <ul className="flex items-center gap-4">
-        <li>
+        {categories.map((category, index)=>(
+            <li key={index}>
           <Button
             variant={"ghost"}
-            className={activeCategory === "All" ? `bg-white` : ``}
+            className={activeCategory === category ? `bg-white` : ``}
             onClick={() => {
-              applyFilter("All");
-              setActiveCategory("All");
+              applyFilter(category);
+              setActiveCategory(category);
             }}
           >
-            All
+            {category}
           </Button>
         </li>
-        <li>
-          <Button
-            variant={"ghost"}
-            className={activeCategory === "Electronics" ? `bg-white` : ``}
-            onClick={() => {
-              applyFilter("Electronics");
-              setActiveCategory("Electronics");
-            }}
-          >
-            Electronics
-          </Button>
-        </li>
-        <li>
-          <Button
-            variant={"ghost"}
-            className={activeCategory === "Accessories" ? `bg-white` : ``}
-            onClick={() => {
-              applyFilter("Accessories");
-              setActiveCategory("Accessories");
-            }}
-          >
-            Accessories
-          </Button>
-        </li>
-        <li>
-          <Button
-            variant={"ghost"}
-            className={activeCategory === "Books" ? `bg-white` : ``}
-            onClick={() => {
-              applyFilter("Books");
-              setActiveCategory("Books");
-            }}
-          >
-            Books
-          </Button>
-        </li>
+        ))}
+        
+        
       </ul>
     </div>
   );
