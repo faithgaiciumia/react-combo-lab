9c7c97ef7cd54e07dacf66c5e49a221e2028abac
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-09-13 10:50:23 +0300

    add fields form input handling and state updates

--- src/apps/form/components/BuilderPanel.tsx
+++ src/apps/form/components/BuilderPanel.tsx
@@ -15,9 +15,52 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Switch } from "@/components/ui/switch"
+import { Switch } from "@/components/ui/switch";
+import { X } from "lucide-react";
+import { useState, type SetStateAction } from "react";
+import type { FormField } from "../page";
 
-export default function BuilderPanel() {
+export default function BuilderPanel({
+  setFormFields,
+}: {
+  setFormFields: React.Dispatch<SetStateAction<FormField[]>>;
+}) {
+  const [type, setType] = useState<"text" | "email" | "number" | "select" | "">(
+    ""
+  );
+  const [label, setLabel] = useState("");
+  const [singleOption, setSingleOption] = useState("");
+  const [options, setOptions] = useState<string[]>([]);
+  const [required, setRequired] = useState(false);
+
+  const handleAddOption = () => {
+    if (!singleOption.trim()) return;
+    setOptions((prev) => [...prev, singleOption.trim()]);
+    setSingleOption("");
+  };
+  const handleOptionRemove = (index: number) => {
+    setOptions((prev) => prev.filter((_, i) => i !== index));
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!type || !label.trim()) return;
+
+    const newField: FormField = {
+      id: crypto.randomUUID(),
+      type,
+      label: label.trim(),
+      options: type === "select" ? options : [],
+      required,
+    };
+    setFormFields((prev) => [...prev, newField]);
+
+    //reset fields
+    setType("");
+    setLabel("");
+    setOptions([]);
+    setSingleOption("");
+    setRequired(false);
+  };
   return (
     <div className="w-[100%] p-2 rounded-md shadow-md">
       <div className="flex justify-between items-center">
@@ -26,15 +69,23 @@ export default function BuilderPanel() {
       </div>
       <div className="flex my-4 justify-center">
         <Dialog>
-          <DialogTrigger asChild><Button className="bg-pink-600 hover:bg-pink-800">Add Field</Button></DialogTrigger>
+          <DialogTrigger asChild>
+            <Button className="bg-pink-600 hover:bg-pink-800">Add Field</Button>
+          </DialogTrigger>
           <DialogContent>
             <DialogHeader>
               <DialogTitle>Add Field to Form</DialogTitle>
             </DialogHeader>
-            <form className="flex gap-4 flex-col">
+            <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
               <div className="flex gap-2 flex-col">
                 <Label>Choose Field Type:</Label>
-                <Select>
+                <Select
+                  value={type}
+                  onValueChange={(val) =>
+                    setType(val as "" | "text" | "number" | "select")
+                  }
+                  required
+                >
                   <SelectTrigger className="w-[100%]">
                     <SelectValue placeholder="Field" />
                   </SelectTrigger>
@@ -48,19 +99,63 @@ export default function BuilderPanel() {
               </div>
               <div className="flex gap-2 flex-col">
                 <Label>Enter Label:</Label>
-                <Input placeholder="eg., Full Name"/>
+                <Input
+                  required
+                  placeholder="eg., Full Name"
+                  value={label}
+                  onChange={(e) => setLabel(e.target.value)}
+                />
               </div>
-              <div className="flex gap-2 flex-col">
-                <Label>Add Options:</Label>
-                <Input />
-                <Button variant={"outline"} className="border-pink-600 text-pink-600 w-[120px]">Add Another</Button>
-              </div>
+              {type === "select" && (
+                <div className="flex gap-2 flex-col">
+                  <Label>Add Options:</Label>
+                  <Input
+                    value={singleOption}
+                    onChange={(e) => setSingleOption(e.target.value)}
+                  />
+                  <Button
+                    variant={"outline"}
+                    className="border-pink-600 text-pink-600 w-[120px]"
+                    onClick={handleAddOption}
+                    type="button"
+                  >
+                    Add Another
+                  </Button>
+                  {options.length > 0 && (
+                    <div className="bg-gray-100 p-2 shadow-sm">
+                      {options.map((option, index) => (
+                        <div
+                          className="flex justify-between items-center"
+                          key={index}
+                        >
+                          <span className="text-sm">{option}</span>
+                          <Button
+                            variant={"ghost"}
+                            onClick={() => handleOptionRemove(index)}
+                          >
+                            <X />
+                          </Button>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              )}
               <div className="flex items-center gap-4">
                 <Label>Required</Label>
-                <Switch/>
+                <Switch
+                  id="required"
+                  checked={required}
+                  onCheckedChange={(val) => setRequired(val)}
+                />
               </div>
               <div className="flex justify-center items-center">
-                <Button className="bg-pink-600 hover:bg-pink-800 w-[200px]">Create Field</Button>
+                <Button
+                  className="bg-pink-600 hover:bg-pink-800 w-[200px]"
+                  type="submit"
+                >
+                  Create Field
+                </Button>
               </div>
             </form>
           </DialogContent>
--- src/apps/form/page.tsx
+++ src/apps/form/page.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import BuilderPanel from "./components/BuilderPanel";
 
+export interface FormField {
+  id: string;
+  type: "text" | "email" | "number" | "select";
+  label: string;
+  required: boolean;
+  options?: string[];
+}
 export default function FormBuilder() {
+  const [formFields, setFormFields] = useState<FormField[]>([]);
   return (
     <div className="p-4">
       <div className="flex justify-center">
         <h1 className="font-bold text-xl">Form Builder</h1>
       </div>
       <div className="grid grid-cols-2 gap-4 my-2">
-        <BuilderPanel/>
+        <BuilderPanel setFormFields={setFormFields} />
         <div>
           <h1>Form Preview</h1>
+          {formFields.map((field, index) => (
+            <h1 key={index}>{field.type}</h1>
+          ))}
         </div>
       </div>
     </div>
