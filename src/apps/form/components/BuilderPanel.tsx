aeaa409ac29ffafcdd0e570c20af93267310b736
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-09-13 14:20:22 +0300

    initialised fields list display

--- src/apps/form/components/BuilderPanel.tsx
+++ src/apps/form/components/BuilderPanel.tsx
@@ -19,11 +19,12 @@ import { Switch } from "@/components/ui/switch";
 import { X } from "lucide-react";
 import { useState } from "react";
 import type { FormField } from "../page";
+import FieldCard from "./FieldCard";
 
 export default function BuilderPanel({
   addFormField,
 }: {
-  addFormField:(newField:FormField)=>void;
+  addFormField: (newField: FormField) => void;
 }) {
   const [type, setType] = useState<"text" | "email" | "number" | "select" | "">(
     ""
@@ -52,7 +53,7 @@ export default function BuilderPanel({
       options: type === "select" ? options : [],
       required,
     };
-    addFormField (newField);
+    addFormField(newField);
 
     //reset fields
     setType("");
@@ -65,101 +66,107 @@ export default function BuilderPanel({
     <div className="w-[100%] p-2 rounded-md shadow-md">
       <div className="flex justify-between items-center">
         <h1 className="font-bold text-md">Builder Panel</h1>
-        <Button>Clear All</Button>
-      </div>
-      <div className="flex my-4 justify-center">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="bg-pink-600 hover:bg-pink-800">Add Field</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Add Field to Form</DialogTitle>
-            </DialogHeader>
-            <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
-              <div className="flex gap-2 flex-col">
-                <Label>Choose Field Type:</Label>
-                <Select
-                  value={type}
-                  onValueChange={(val) =>
-                    setType(val as "" | "text" | "number" | "select")
-                  }
-                  required
-                >
-                  <SelectTrigger className="w-[100%]">
-                    <SelectValue placeholder="Field" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="text">Text</SelectItem>
-                    <SelectItem value="email">Email</SelectItem>
-                    <SelectItem value="number">Number</SelectItem>
-                    <SelectItem value="select">Select</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="flex gap-2 flex-col">
-                <Label>Enter Label:</Label>
-                <Input
-                  required
-                  placeholder="eg., Full Name"
-                  value={label}
-                  onChange={(e) => setLabel(e.target.value)}
-                />
-              </div>
-              {type === "select" && (
+        <div className="flex gap-2">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="bg-pink-600 hover:bg-pink-800">
+                Add Field
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Field to Form</DialogTitle>
+              </DialogHeader>
+              <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
                 <div className="flex gap-2 flex-col">
-                  <Label>Add Options:</Label>
-                  <Input
-                    value={singleOption}
-                    onChange={(e) => setSingleOption(e.target.value)}
-                  />
-                  <Button
-                    variant={"outline"}
-                    className="border-pink-600 text-pink-600 w-[120px]"
-                    onClick={handleAddOption}
-                    type="button"
+                  <Label>Choose Field Type:</Label>
+                  <Select
+                    value={type}
+                    onValueChange={(val) =>
+                      setType(val as "" | "text" | "number" | "select")
+                    }
+                    required
                   >
-                    Add Another
-                  </Button>
-                  {options.length > 0 && (
-                    <div className="bg-gray-100 p-2 shadow-sm">
-                      {options.map((option, index) => (
-                        <div
-                          className="flex justify-between items-center"
-                          key={index}
-                        >
-                          <span className="text-sm">{option}</span>
-                          <Button
-                            variant={"ghost"}
-                            onClick={() => handleOptionRemove(index)}
-                          >
-                            <X />
-                          </Button>
-                        </div>
-                      ))}
-                    </div>
-                  )}
+                    <SelectTrigger className="w-[100%]">
+                      <SelectValue placeholder="Field" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="text">Text</SelectItem>
+                      <SelectItem value="email">Email</SelectItem>
+                      <SelectItem value="number">Number</SelectItem>
+                      <SelectItem value="select">Select</SelectItem>
+                    </SelectContent>
+                  </Select>
                 </div>
-              )}
-              <div className="flex items-center gap-4">
-                <Label>Required</Label>
-                <Switch
-                  id="required"
-                  checked={required}
-                  onCheckedChange={(val) => setRequired(val)}
-                />
-              </div>
-              <div className="flex justify-center items-center">
-                <Button
-                  className="bg-pink-600 hover:bg-pink-800 w-[200px]"
-                  type="submit"
-                >
-                  Create Field
-                </Button>
-              </div>
-            </form>
-          </DialogContent>
-        </Dialog>
+                <div className="flex gap-2 flex-col">
+                  <Label>Enter Label:</Label>
+                  <Input
+                    required
+                    placeholder="eg., Full Name"
+                    value={label}
+                    onChange={(e) => setLabel(e.target.value)}
+                  />
+                </div>
+                {type === "select" && (
+                  <div className="flex gap-2 flex-col">
+                    <Label>Add Options:</Label>
+                    <Input
+                      value={singleOption}
+                      onChange={(e) => setSingleOption(e.target.value)}
+                    />
+                    <Button
+                      variant={"outline"}
+                      className="border-pink-600 text-pink-600 w-[120px]"
+                      onClick={handleAddOption}
+                      type="button"
+                    >
+                      Add Another
+                    </Button>
+                    {options.length > 0 && (
+                      <div className="bg-gray-100 p-2 shadow-sm">
+                        {options.map((option, index) => (
+                          <div
+                            className="flex justify-between items-center"
+                            key={index}
+                          >
+                            <span className="text-sm">{option}</span>
+                            <Button
+                              variant={"ghost"}
+                              onClick={() => handleOptionRemove(index)}
+                            >
+                              <X />
+                            </Button>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                )}
+                <div className="flex items-center gap-4">
+                  <Label>Required</Label>
+                  <Switch
+                    id="required"
+                    checked={required}
+                    onCheckedChange={(val) => setRequired(val)}
+                  />
+                </div>
+                <div className="flex justify-center items-center">
+                  <Button
+                    className="bg-pink-600 hover:bg-pink-800 w-[200px]"
+                    type="submit"
+                  >
+                    Create Field
+                  </Button>
+                </div>
+              </form>
+            </DialogContent>
+          </Dialog>
+          <Button>Clear All</Button>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-2 my-4">
+        <FieldCard/>
+        <FieldCard/>
       </div>
     </div>
   );
---
+++ src/apps/form/components/FieldCard.tsx
@@ -0,0 +1,12 @@
+export default function FieldCard() {
+  return (
+    <div className="border-gray-500 border-1 rounded-md p-4">
+      <div>
+        <h1>Email Input</h1>
+        <h2>Type: Email</h2>
+        <h3>Options:</h3>
+        <h4>Required</h4>
+      </div>
+    </div>
+  );
+}
